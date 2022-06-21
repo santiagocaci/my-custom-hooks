1f91dbe448ce8515188565d3500956ff6c9c9e67
Santiago Caci <santiago.caci@gmail.com>
2022-06-21 16:25:23 -0300

    first commit

---
+++ useTodo.js
@@ -0,0 +1,56 @@
+import { useEffect, useReducer } from 'react';
+import { todoReducer } from '../08-useReducer/todoReducer';
+
+// const initialState = [];
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('todos')) || [];
+};
+
+export const useTodo = () => {
+  const [todos, dispatch] = useReducer(todoReducer, [], init);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewTodo = (todo) => {
+    const action = {
+      type: '[TODO] Add Todo',
+      payload: todo,
+    };
+    dispatch(action);
+  };
+
+  const handleDeleteTodo = (id) => {
+    const action = {
+      type: '[TODO] Remove Todo',
+      payload: id,
+    };
+    dispatch(action);
+  };
+
+  const handleToggleTodo = (id) => {
+    dispatch({
+      type: '[TODO] Toggle Todo',
+      payload: id,
+    });
+  };
+
+  const todosCount = () => {
+    return todos.length;
+  };
+
+  const pendingTodosCount = () => {
+    return todos.filter((todo) => !todo.done).length;
+  };
+
+  return {
+    todos,
+    todosCount,
+    pendingTodosCount,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleToggleTodo,
+  };
+};
